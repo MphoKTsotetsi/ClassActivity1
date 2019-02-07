eaaeced7d3ea8654155c2577727a9134fa602f73
unknown <User@INFBN-80.up.ac.za>
2019-02-07 17:04:05 +0200

    resolve issues

--- Site.js
+++ Site.js
@@ -1,6 +1,6 @@
 <script>
 function myClicker() 
 {
-alert("Im so excited about this module because i want to be super read for the work environment.")
+alert("Im so excited about this module because i want to be super ready for the work environment.");
 }
 </script>
