ee49856a127c650c261cdaa7e22194bb42d8eeca
unknown <User@INFBN-78.up.ac.za>
2019-02-07 16:17:51 +0200

    Student number, name and motivation

--- Site.js
+++ Site.js
@@ -1,1 +1,6 @@
-hdsagbhkabbhfk;ad
+
+function myFunction() {
+  alert("I am excited to do INF 354 because I think it will equip me properly for the workplace and I am able to advance my skills as an individual");
+}
+
+
