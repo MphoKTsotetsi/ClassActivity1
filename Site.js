4b7412655be2b2a28911399867fc88e3289063e1
unknown <User@INFBN-79.up.ac.za>
2019-02-07 16:42:24 +0200

    conflict hungry and code

--- Site.js
+++ Site.js
@@ -1,1 +1,3 @@
-hdsagbhkabbhfk;ad
+function myfunction() {
+alert("it seems like an interesting module with a lot of things which i am looking forward to see, such as Angular and Phython as well as ionic");
+}
